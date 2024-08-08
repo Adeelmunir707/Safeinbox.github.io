77614b1fb91d04d6aaea18a9a3b55f8c88dcd9d6
Adeel-Munir <111064925+Adeelmunir707@users.noreply.github.com>
2024-08-08 12:18:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pickle
 import string
 from string import punctuation
-from nltk.corpus import stopwords
-nltk.download('stopwords')
+
 from nltk.tokenize import word_tokenize
 import nltk
 from nltk.stem.porter import PorterStemmer
@@ -26,8 +25,18 @@ def transform_text(text):
     y.clear()
 
     for i in text:
-
-        if i not in stopwords.words('english') and i not in string.punctuation:
+stopwords = set([
+    'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves',
+    'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their',
+    'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was',
+    'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and',
+    'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between',
+    'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off',
+    'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any',
+    'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so',
+    'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now'
+])
+        if i not in stopwords and i not in string.punctuation:
             y.append(i)
 
     text = y[:]
