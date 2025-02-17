a98d28b5cd2b75a2a3a802a23106b7cc48c0c0ec
Adeel-Munir <111064925+Adeelmunir707@users.noreply.github.com>
2025-02-17 21:02:07 +0500

    Update app.py

--- app.py
+++ app.py
@@ -82,4 +82,6 @@ st.write("\n\n\n\n\n\n\n\n\n")
 st.write("")
 st.write("")
 st.write("")
-st.write("\t\t\t\t\t\tCopy© 2025 Adeel Munir | Made With ❤️ in Pakistan")
+st.markdown("<hr style='border: 2px solid black;'>", unsafe_allow_html=True)
+# Footer content
+st.write("Copy© 2025 Adeel Munir | Made With ❤️ in Pakistan")
