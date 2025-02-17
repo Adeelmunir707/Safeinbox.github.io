ee6e25edcd4202c65da9265c0f0cf0619dc11842
Adeel-Munir <111064925+Adeelmunir707@users.noreply.github.com>
2025-02-17 21:02:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -79,9 +79,8 @@ if st.button('Inquire'):
 
 st.write("\n\n\n\n\n\n\n\n\n")
 
-st.write("")
-st.write("")
-st.write("")
+st.write("\n" * 15)
+# Add a bold line above the footer
 st.markdown("<hr style='border: 2px solid black;'>", unsafe_allow_html=True)
 # Footer content
 st.write("Copy© 2025 Adeel Munir | Made With ❤️ in Pakistan")
